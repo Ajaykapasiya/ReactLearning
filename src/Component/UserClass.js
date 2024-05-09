7abd09fba761f09813517fe2924336edbe202f3e
ajay <ajaykumar290904@gmail.com>
2024-05-09 18:18:03 +0530

    how to fetch API in class based component using the componentDidMount

--- src/Component/UserClass.js
+++ src/Component/UserClass.js
@@ -9,32 +9,47 @@ class UserClass extends React.Component {
         console.log(props)
 
         this.state = {
-            count: 0
+            userInfo: {
+                name: "Ajay kapasiya ", location: "India", age: 22, id: 3423, avatar_url: "http://dummy-photo.com"
+
+            }
         };
     }
-      async componentDidMount()
-    {
-const data = await fetch("https://api.github.com/users/Ajaykapasiya")
+
+    async componentDidMount() {
+        const data = await fetch("https://api.github.com/users/Ajaykapasiya")
         const json = await data.json();
-        console.log  (json);
+
+        this.setState({
+            userInfo: json,
+        });
+
+        console.log(json);
+
     }
 
+
     render() {
-         const {name} = this.props;
-         const {count} = this.state;
+        const {count} = this.state;
+        const {
+            name, location, age, id, avatar_url
+        } = this.state.userInfo
 
         return (<div className="user-card">
             <h1>count:{count}</h1>
             <button
                 onClick={() => {
-                this.setState({
-                    count: this.state.count + 1,
-                });
-            }}>
+                    this.setState({
+                        count: this.state.count + 1,
+                    });
+                }}>
                 count increase
             </button>
+            <img src={avatar_url}/>
             <h2>Name:{name}</h2>
-            <h2>Age:21</h2>
+            <h2>Location:{location}</h2>
+            <h2>Age:{age}</h2>
+            <h2>id:{id}</h2>
             <h2>course:Btech</h2>
             <h2>Contact:997*******</h2>
 
