567fa5f499f9b2f81519ff91979c3e7dc7eaca80
ajay <ajaykumar290904@gmail.com>
2024-07-01 23:45:03 +0530

    high order component

--- src/Component/Body.js
+++ src/Component/Body.js
@@ -10,7 +10,7 @@ const Body = () => {
     const [searchText, setSearchText] = useState("");
 
     useEffect(() => {
-        console.log("render")
+        console.log("render" , listOfRestaurants)
         fetchData();
     }, []);
 
